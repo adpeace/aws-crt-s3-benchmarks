21d3d3112a09e77c1aef5de0a9e924fb343e5992
Waqar Ahmed Khan <waahm7@gmail.com>
2024-05-23 21:40:45 +0000

    fixes

--- testRunBenchmarksInParallel.sh
+++ testRunBenchmarksInParallel.sh
@@ -4,32 +4,32 @@
 CMD_1="/usr/bin/python3 /home/ec2-user/work/aws-crt-s3-benchmarks/scripts/run-benchmarks.py \
   --runner-cmd /home/ec2-user/work/aws-crt-s3-benchmarks/build/c/install/bin/s3-benchrunner-c \
   --s3-client crt-c \
-  --bucket waqar-aws-c-s3-test-bucket \
-  --region us-west-2 \
+  --bucket waqar-us-east-1-multiple-nic-test \
+  --region us-east-1 \
   --throughput 100.0 \
   --files-dir /home/ec2-user/work/aws-crt-s3-benchmarks/files \
   --workloads /home/ec2-user/work/aws-crt-s3-benchmarks/workloads/download-100GiB-1x-ram-1.run.json"
 CMD_2="/usr/bin/python3 /home/ec2-user/work/aws-crt-s3-benchmarks/scripts/run-benchmarks.py \
   --runner-cmd /home/ec2-user/work/aws-crt-s3-benchmarks/build/c/install/bin/s3-benchrunner-c \
   --s3-client crt-c \
-  --bucket waqar-aws-c-s3-test-bucket \
-  --region us-west-2 \
+  --bucket waqar-us-east-1-multiple-nic-test \
+  --region us-east-1 \
   --throughput 100.0 \
   --files-dir /home/ec2-user/work/aws-crt-s3-benchmarks/files \
   --workloads /home/ec2-user/work/aws-crt-s3-benchmarks/workloads/download-100GiB-1x-ram-2.run.json"
 CMD_3="/usr/bin/python3 /home/ec2-user/work/aws-crt-s3-benchmarks/scripts/run-benchmarks.py \
   --runner-cmd /home/ec2-user/work/aws-crt-s3-benchmarks/build/c/install/bin/s3-benchrunner-c \
   --s3-client crt-c \
-  --bucket waqar-aws-c-s3-test-bucket \
-  --region us-west-2 \
+  --bucket waqar-us-east-1-multiple-nic-test \
+  --region us-east-1 \
   --throughput 100.0 \
   --files-dir /home/ec2-user/work/aws-crt-s3-benchmarks/files \
   --workloads /home/ec2-user/work/aws-crt-s3-benchmarks/workloads/download-100GiB-1x-ram-3.run.json"
 CMD_4="/usr/bin/python3 /home/ec2-user/work/aws-crt-s3-benchmarks/scripts/run-benchmarks.py \
   --runner-cmd /home/ec2-user/work/aws-crt-s3-benchmarks/build/c/install/bin/s3-benchrunner-c \
   --s3-client crt-c \
-  --bucket waqar-aws-c-s3-test-bucket \
-  --region us-west-2 \
+  --bucket waqar-us-east-1-multiple-nic-test \
+  --region us-east-1 \
   --throughput 100.0 \
   --files-dir /home/ec2-user/work/aws-crt-s3-benchmarks/files \
   --workloads /home/ec2-user/work/aws-crt-s3-benchmarks/workloads/download-100GiB-1x-ram-4.run.json"
@@ -52,10 +52,10 @@ trap cleanup SIGINT SIGTERM
 start_time=$(date +%s)
 
 # Loop over workloads and run each command in parallel
-BIND_ADDR="172.31.83.94" LD_PRELOAD=/home/ec2-user/work/bindhack/bind.so $CMD_1 &
-BIND_ADDR="172.31.82.67" LD_PRELOAD=/home/ec2-user/work/bindhack/bind.so $CMD_2 &
-BIND_ADDR="172.31.83.94" LD_PRELOAD=/home/ec2-user/work/bindhack/bind.so $CMD_3 &
-BIND_ADDR="172.31.82.67" LD_PRELOAD=/home/ec2-user/work/bindhack/bind.so $CMD_4 &
+#BIND_ADDR="172.31.83.94" LD_PRELOAD=/home/ec2-user/work/aws-crt-s3-benchmarks/bind.so $CMD_1 &
+BIND_ADDR="172.31.82.67" LD_PRELOAD=/home/ec2-user/work/aws-crt-s3-benchmarks/bind.so $CMD_2 &
+#BIND_ADDR="172.31.83.94" LD_PRELOAD=/home/ec2-user/work/aws-crt-s3-benchmarks/bind.so $CMD_3 &
+#BIND_ADDR="172.31.82.67" LD_PRELOAD=/home/ec2-user/work/aws-crt-s3-benchmarks/bind.so $CMD_4 &
 
 # Wait for all background processes to finish
 wait
